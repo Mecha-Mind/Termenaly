140517145bd3e246ff8f34a5d8b4b7d2da3c5327
Mecha <hanishetos4@gmail.com>
2022-10-23 18:26:59 +0200

    Added Term Files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let holidays = 0,
+    money = 0;
+if (holidays == 0) {
+    money = 5000;
+    console.log(`Your Money is ${money}`);
+} else if (holidays == 1 || holidays == 2) {
+    money = 3000;
+    console.log(`Your Money is ${money}`);
+} else if (holidays == 3) {
+    money = 2000;
+    console.log(`Your Money is ${money}`);
+}
